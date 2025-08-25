a5d76a7d110080e3d0c487686672aeb88504b8fa
RollendeTonne <huegelschaefer@gmail.com>
2025-08-25 21:24:23 +0200

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,9 +1,6 @@
 input.onSound(DetectedSound.Loud, function () {
     music.playMelody("C5 E A D G C D D ", 120)
     basic.setLedColors(0x0000ff, 0x7f00ff, 0x007fff)
-    if (maqueen.irRead() == 0) {
-        basic.turnRgbLedOff()
-    }
 })
 input.onButtonEvent(Button.A, input.buttonEventClick(), function () {
     maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 100)
@@ -23,4 +20,15 @@ input.onButtonEvent(Button.A, input.buttonEventClick(), function () {
     maqueen.motorStop(maqueen.Motors.All)
     maqueen.writeLED(maqueen.Led.LedAll, maqueen.LedSwitch.LedOff)
     maqueen.setColor(0x000000)
+})
+input.onButtonEvent(Button.B, input.buttonEventClick(), function () {
+	
+})
+basic.forever(function () {
+    serial.writeLine("" + (maqueen.irRead()))
+})
+basic.forever(function () {
+    if (maqueen.irRead() == 175) {
+        basic.turnRgbLedOff()
+    }
 })
