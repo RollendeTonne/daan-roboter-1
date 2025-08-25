2e95474c65b2393877bb7863a3315b7d07e51bc2
RollendeTonne <huegelschaefer@gmail.com>
2025-08-25 21:54:43 +0200

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,11 +1,7 @@
-input.onSound(DetectedSound.Loud, function () {
-    music.playMelody("C5 E A D G C D D ", 120)
-    basic.setLedColors(0x0000ff, 0x7f00ff, 0x007fff)
-})
-input.onButtonEvent(Button.A, input.buttonEventClick(), function () {
+input.onSound(DetectedSound.Quiet, function () {
+    music.play(music.stringPlayable("E - B - A C5 F G ", 40), music.PlaybackMode.InBackground)
     maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 100)
     basic.pause(1000)
-    maqueen.motorStop(maqueen.Motors.All)
     basic.pause(1000)
     maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CCW, 100)
     basic.pause(1000)
@@ -13,13 +9,69 @@ input.onButtonEvent(Button.A, input.buttonEventClick(), function () {
     maqueen.setColor(0x0000ff)
     maqueen.writeLED(maqueen.Led.LedAll, maqueen.LedSwitch.LedOn)
     basic.pause(1000)
-    maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 100)
+    maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 255)
     basic.pause(1000)
-    maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CCW, 100)
+    maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CCW, 255)
+    basic.pause(1000)
+    maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 200)
     basic.pause(1000)
     maqueen.motorStop(maqueen.Motors.All)
     maqueen.writeLED(maqueen.Led.LedAll, maqueen.LedSwitch.LedOff)
     maqueen.setColor(0x000000)
+    maqueen.motorStop(maqueen.Motors.All)
+    basic.showLeds(`
+        # . # . #
+        . # . # .
+        # . # . #
+        . # . # .
+        # . # . #
+        `)
+    basic.showLeds(`
+        . . . . .
+        . # . # .
+        . . # . .
+        . # . # .
+        . . . . .
+        `)
+    basic.showLeds(`
+        # . # . #
+        . # . # .
+        # . . . #
+        . # . # .
+        # . # . #
+        `)
+    basic.showLeds(`
+        # # # # #
+        # . . . #
+        # . . . #
+        # . . . #
+        # # # # #
+        `)
+    basic.showLeds(`
+        . . . . .
+        . # # # .
+        . # # # .
+        . # # # .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . # # # .
+        . # # # .
+        . . . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+})
+input.onSound(DetectedSound.Loud, function () {
+    music.playMelody("C5 E A D G C D D ", 120)
+    basic.setLedColors(0x0000ff, 0x7f00ff, 0x007fff)
 })
 input.onButtonEvent(Button.B, input.buttonEventClick(), function () {
 	
